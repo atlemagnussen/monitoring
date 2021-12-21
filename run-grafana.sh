fa456c9a10a9a14f085e31732be7a9624e97f99e
Atle Magnussen <atlemagnussen@gmail.com>
2021-12-21 21:00:56 +0100

    added azure plugin

--- run-grafana.sh
+++ run-grafana.sh
@@ -1,4 +1,10 @@
 
 #!/usr/bin/env bash
 
-docker run --user $(id -u):$(id -g) -d --volume "/mnt/md1/Data/grafana/data:/var/lib/grafana" -p 3000:3000 grafana/grafana-oss
+docker container rm grafana-atle
+
+docker run --user $(id -u):$(id -g) \
+    --name grafana-atle \
+    --volume "/mnt/md1/Data/grafana/data:/var/lib/grafana" \
+    -e "GF_INSTALL_PLUGINS=grafana-azure-monitor-datasource" \
+    -p 3000:3000 -d grafana/grafana-oss
