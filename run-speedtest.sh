2ad73aeca8d5cf0b1ab12f6c2b9dbe27d3670059
Atle Magnussen <atlemagnussen@gmail.com>
2021-12-21 20:52:20 +0100

    add router exporter

--- run-speedtest.sh
+++ run-speedtest.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 docker run --rm -p 9876:9876 -d caarlos0/speedtest-exporter
+
+docker run -d \
+  --name=speedtest-exporter \
+  -p 9798:9798 \
+  --restart unless-stopped \
+  miguelndecarvalho/speedtest-exporter
