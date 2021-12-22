c278c5ae208306f3fec22518c341983975312744
Atle Magnussen <atlemagnussen@gmail.com>
2021-12-22 22:15:05 +0100

    fix

--- run-prometheus.sh
+++ run-prometheus.sh
@@ -2,6 +2,7 @@
 #!/usr/bin/env bash
 
 docker run --user $(id -u):$(id -g) -d \
+    --name prometheus-atle \
     -p 9090:9090 \
     -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml \
     -v /mnt/md1/Data/prometheus/data:/prometheus:z \
