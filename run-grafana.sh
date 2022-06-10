7020f8ed41f4467ffbc0c7ba5f8937a9d2452630
Atle Magnussen <atlemagnussen@gmail.com>
2022-06-10 10:00:37 +0200

    rem azure plugin

--- run-grafana.sh
+++ run-grafana.sh
@@ -6,5 +6,4 @@ docker container rm grafana-atle
 docker run --user $(id -u):$(id -g) \
     --name grafana-atle \
     --volume "/mnt/md1/Data/grafana/data:/var/lib/grafana" \
-    -e "GF_INSTALL_PLUGINS=grafana-azure-monitor-datasource" \
-    -p 3000:3000 -d grafana/grafana-oss
+    -p 3000:3000 -d grafana/grafana-oss:8.5.5
